43c7a40e566932bdb66ed822c4cbf9bd86bf1784
s21355 <56189684+s21355@users.noreply.github.com>
2022-06-18 11:49:14 +0200

    Initial commit

---
+++ src/main/java/pl/pjat/jaz21355nbp/Jaz21355NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjat.jaz21355nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz21355NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaz21355NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjat/jaz21355nbp/Jaz21355NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjat.jaz21355nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz21355NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
